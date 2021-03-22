092cc92a1e7ea2fac3a71fedd1399461a9ff6ac8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-03-22 08:54:24 -0300

    Parte 8 da aula 1 do curso Fundamentos de Flutter: Crie o seu primeiro App

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,31 +2,23 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Column(
-    children: <Widget>[
-      Text(
-        'Deliver features faster',
-        textDirection: TextDirection.ltr,
-      ),
-      Text(
-        'Craft beautiful UIs',
-        textDirection: TextDirection.ltr,
-      ),
-      Expanded(
-        child: FittedBox(
-          fit: BoxFit.contain, // otherwise the logo will be tiny
-          child: const FlutterLogo(),
+void main() => runApp(Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
         ),
-      ),
-      Column(
-        children: <Widget>[
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-        ],
-      )
-    ],
-  ));
-}
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        )
+      ],
+    ));
