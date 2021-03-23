b1c0def63b92f2609b9929525f1b51e574b09bbf
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-03-23 14:52:25 -0300

    Parte 5 da aula 2 do curso Fundamentos de Flutter: Crie o seu primeiro App

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -8,5 +8,21 @@ void main() => runApp(MaterialApp(
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
         ),
+        body: Column(
+          children: [
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text("R\$ 1000.00"),
+              subtitle: Text("37890"),
+            )),
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text("R\$ 800.00"),
+              subtitle: Text("379990"),
+            ))
+          ],
+        ),
       ),
     ));
