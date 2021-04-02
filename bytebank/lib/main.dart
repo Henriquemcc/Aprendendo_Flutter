0550045164034ccca89d8be66f000c9d299d5487
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-04-02 10:28:37 -0300

    Parte 3 da aula 3 do curso Fundamentos de Flutter: Crie o seu primeiro App

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -23,7 +23,33 @@ class FormularioTransferencia extends StatelessWidget {
         appBar: AppBar(
           title: Text("Criando transferência"),
         ),
-        body: Text("teste"));
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    labelText: 'Número da conta', hintText: '0000'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0000'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: Text("Confirmar"),
+            ),
+          ],
+        ));
   }
 }
 
